34499ff2bf3a997802b86297534d8d2a60189546
sok-leaphea <leapheas2@gmail.com>
2024-03-06 16:41:10 +0700

    Translate

--- bt_mrd/bt_mdr/bt_mdr_project/bt_mdr_project/settings.py
+++ bt_mrd/bt_mdr/bt_mdr_project/bt_mdr_project/settings.py
@@ -106,19 +106,19 @@ AUTH_USER_MODEL = 'mdrapp.User'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rwsdm',
-        'USER': 'postgres',
-        'PASSWORD': 'rootuser',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'rwsdm',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'rootuser',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'br_mrd',
@@ -258,7 +258,7 @@ EASY_MAPS_CENTER = (-41.3, 32)
 #CORS_ALLOWED_ORIGINS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
-#API_ENDPOINT = "http://127.0.0.1:8000/en/api/"
+# API_ENDPOINT = "http://127.0.0.1:8000/en/api/"
 # API_ENDPOINT = "http://18.222.12.231/en/api/"
 API_ENDPOINT = "http://3.0.166.20//en/api/"
 
--- bt_mrd/bt_mdr/bt_mdr_project/settings.py
+++ bt_mrd/bt_mdr/bt_mdr_project/settings.py
@@ -22,80 +22,81 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_trh!c6t8s(rce#*ccp#v))v4qxg)g8(b#u%k+p37opz91#+j1'
+SECRET_KEY = "django-insecure-_trh!c6t8s(rce#*ccp#v))v4qxg)g8(b#u%k+p37opz91#+j1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "192.168.0.121", "127.0.0.1", "52.77.223.16", '13.212.250.28']
+ALLOWED_HOSTS = [
+    "localhost",
+    "192.168.0.121",
+    "127.0.0.1",
+    "52.77.223.16",
+    "13.212.250.28",
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mdrapp',
-    'myapi',
-    'knox',
-    'report',
-    'rosetta',  # NEW
-    'parler',
+    "rest_framework",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "mdrapp",
+    "myapi",
+    "knox",
+    "report",
+    "rosetta",  # NEW
+    "parler",
     # 'django_filters',
-    'django_filters',
-    
+    "django_filters",
     # 'django-advanced-filters',
-    'django_google_maps',
+    "django_google_maps",
     #'qrcodeapp',
     # 'mapwidgets',
-    'easy_maps',
-    'corsheaders',
-    
+    "easy_maps",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware', # new
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",  # new
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     # 'django.middleware.common.CommonMiddleware',
-    
 ]
 
-ROOT_URLCONF = 'bt_mdr_project.urls'
+ROOT_URLCONF = "bt_mdr_project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
         # 'DIRS': [],
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'bt_mdr_project.wsgi.application'
+WSGI_APPLICATION = "bt_mdr_project.wsgi.application"
 
-AUTH_USER_MODEL = 'mdrapp.User'
+AUTH_USER_MODEL = "mdrapp.User"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -105,20 +106,20 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rwsdm',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "rwsdm",
+        "USER": "postgres",
+        "PASSWORD": "rootuser",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'br_mrd',
     #     'USER': 'postgres',
     #     'PASSWORD': 'myPassword',
-    #     'HOST': '52.14.59.145', 
+    #     'HOST': '52.14.59.145',
     #     'PORT': '5432',
     # }
 }
@@ -129,24 +130,24 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'knox.auth.TokenAuthentication',
+        "knox.auth.TokenAuthentication",
     ],
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework.authentication.TokenAuthentication',
@@ -156,74 +157,73 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-        )
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
 
-CSRF_COOKIE_SAMESITE = 'Strict'
-SESSION_COOKIE_SAMESITE = 'Strict'
+CSRF_COOKIE_SAMESITE = "Strict"
+SESSION_COOKIE_SAMESITE = "Strict"
 CSRF_COOKIE_HTTPONLY = False  # False since we will grab it via universal-cookies
 SESSION_COOKIE_HTTPONLY = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-#LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'km'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "km"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
 LANGUAGES = (
-    ('km', _('Khmer')),
-    ('en', _('English')),
-    ('fr', _('French')),
-    ('th', _('Thai')),
+    ("km", _("Khmer")),
+    ("en", _("English")),
+    ("fr", _("French")),
+    ("th", _("Thai")),
     # ('es', _('Spanish')),
-    
 )
 # LOCALE_PATHS = [
 #     BASE_DIR / 'locale/',
 # ]
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, "locale"),
-)
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 PARLER_LANGUAGES = {
     None: (
-        {'code' : 'km'},
-        {'code': 'en',}, # English
-        {'code': 'fr',}, # French
+        {"code": "km"},
+        {
+            "code": "en",
+        },  # English
+        {
+            "code": "fr",
+        },  # French
         # {'code': 'es',}, # Spanish
         # {'code' : 'km'},
-        {'code':'th'},
+        {"code": "th"},
     ),
-    'default': {
-        'fallbacks': ['km'],
-        'hide_untranslated': False,
-    }
+    "default": {
+        "fallbacks": ["km"],
+        "hide_untranslated": False,
+    },
 }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 # Map settings
-GOOGLE_API_KEY = 'AIzaSyB3_FbMwrwR--w2MO21lE39MolKf0AmGx8'
-EASY_MAPS_GOOGLE_KEY = 'AIzaSyB3_FbMwrwR--w2MO21lE39MolKf0AmGx8'
+GOOGLE_API_KEY = "AIzaSyB3_FbMwrwR--w2MO21lE39MolKf0AmGx8"
+EASY_MAPS_GOOGLE_KEY = "AIzaSyB3_FbMwrwR--w2MO21lE39MolKf0AmGx8"
 EASY_MAPS_CENTER = (-41.3, 32)
 # MAP_WIDGETS = {
 #     "GooglePointFieldWidget": (
@@ -235,9 +235,8 @@ EASY_MAPS_CENTER = (-41.3, 32)
 #     "GOOGLE_MAP_API_KEY": GOOGLE_MAP_API_KEY
 # }
 
-#CORS_ALLOWED_ORIGINS = ['*']
+# CORS_ALLOWED_ORIGINS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 API_ENDPOINT = "http://127.0.0.1:8000/en/api/"
-#API_ENDPOINT = "http://3.0.166.20/en/api/"
-
+# API_ENDPOINT = "http://3.0.166.20/en/api/"
